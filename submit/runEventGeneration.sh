77de3b96e1d53cb9ca311444cc7b9cb389965efa
SiewYan <siew.yan.hoh@cern.ch>
2020-08-13 19:05:03 +0200

    update hadronizer

--- submit/runEventGeneration.sh
+++ submit/runEventGeneration.sh
@@ -144,7 +144,7 @@ cmsDriver.py step1 \
     --datatier GEN-SIM-RAW \
     --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 \
     --era Run2_2016 \
-    --nThreads 4 \
+    --nThreads 2 \
     --python_filename ${outfilename}_1_cfg.py \
     --datamix PreMix \
     --pileup_input "dbs:/Neutrino_E-10_gun/RunIISpring15PrePremix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v2-v2/GEN-SIM-DIGI-RAW" \
@@ -162,7 +162,7 @@ cmsDriver.py step2 \
     --datatier AODSIM \
     --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 \
     --era Run2_2016 \
-    --nThreads 4 \
+    --nThreads 2 \
     --python_filename ${outfilename}_2_cfg.py \
     --customise Configuration/DataProcessing/Utils.addMonitoring \
     --runUnscheduled \
@@ -197,7 +197,7 @@ cmsDriver.py step1 \
     --datatier MINIAODSIM \
     --conditions 94X_mcRun2_asymptotic_v3 \
     --era Run2_2016,run2_miniAOD_80XLegacy \
-    --nThreads 4 \
+    --nThreads 2 \
     --python_filename ${outfilename}_miniaod_cfg.py \
     --customise Configuration/DataProcessing/Utils.addMonitoring \
     --runUnscheduled \
@@ -238,7 +238,7 @@ cmsDriver.py step1 \
     --datatier NANOAODSIM \
     --conditions 102X_mcRun2_asymptotic_v6 \
     --era Run2_2016,run2_nanoAOD_94X2016 \
-    --nThreads 4 \
+    --nThreads 2 \
     --python_filename ${outfilename}_nanoaod_cfg.py \
     --customise_commands="process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)))" \
     --customise_commands 'process.particleLevelSequence.remove(process.genParticles2HepMCHiggsVtx);process.particleLevelSequence.remove(process.rivetProducerHTXS);process.particleLevelTables.remove(process.HTXSCategoryTable)' \
